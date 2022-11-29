bc2e64ba69e4e5070730a3d2333c2d155247599a
unknown <bangphan3031@gmail.com>
2022-11-29 21:28:21 +0700

    update views/admin

---
+++ application/views/infor/infor.php
@@ -0,0 +1,24 @@
+<h1 align=center>Thông tin cá nhân</h1>
+<table align="center">
+    <tr>
+        <td>Tên</td>
+        <td>MSSV</td>
+    </tr>
+    <tr>
+        <td>Phan Lương Bằng</td>
+        <td>61130045</td>
+    </tr>
+    <tr>
+        <td>Trương Tấn Nghĩa</td>
+        <td>61130045</td>
+    </tr>
+    <tr>
+        <td>Nguyễn Anh Phương</td>
+        <td>61130045</td>
+    </tr>
+    <tr>
+        <td>Nguyễn Đình Vũ</td>
+        <td>61130045</td>
+    </tr>
+</table>
+<?php echo $this->pagination->create_links(); ?>
